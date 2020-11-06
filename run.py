c6756df7230998fec10772e1a0affb86b3bc5c79
Xia <50060023+xia0405@users.noreply.github.com>
2020-11-06 09:53:48 +0100

    delete some rows

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import uvicorn 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, debug=True)    #host='0.0.0.0', port=8080,
+    uvicorn.run("app:app", reload=True, debug=True)    #host='0.0.0.0', port=8080,
