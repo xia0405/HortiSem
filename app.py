ae8c2db888a2399c6731d1a3ed84ad1401ea71a2
Xia <50060023+xia0405@users.noreply.github.com>
2020-11-17 14:08:55 +0100

    add rule based model

--- app.py
+++ app.py
@@ -8,17 +8,13 @@ import os
 import re
 import shutil
 import csv
-from pathlib import Path
 # import base64
 import slate3k as slate
-from ml import nlp
 
-from spacy.matcher import PhraseMatcher
-from spacy.tokens import Span
+import spacy
 
 from typing import Optional
 from typing import List
-
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi import Request
@@ -36,59 +32,13 @@ templates = Jinja2Templates(directory="templates")
 
 
 UPLOAD_FOLDER = r"M:\Projekt\HortiSem\static\upload_folder"
+
+# ML model
+# nlp = spacy.load("M:/Projekt/HortiSem/tmp_model_v1")
+
+# rule model using statistics
+nlp = spacy.load("M:/Projekt/HortiSem/rule_model")
 # ALLOWED_EXTENTIONS = {'pdf'}
-class EntityMatcher(object):
-    # name = "entity_matcher"
-
-    def __init__(self, nlp, terms, label):
-        patterns = [nlp.make_doc(text) for text in terms]
-        self.matcher = PhraseMatcher(nlp.vocab)
-        self.matcher.add(label, None, *patterns)
-
-    def __call__(self, doc):
-        matches = self.matcher(doc)
-        for match_id, start, end in matches:
-            span = Span(doc, start, end, label=match_id)
-            doc.ents = list(doc.ents) + [span]
-        return doc
-
-# define the file path
-base_path = Path("M:\Projekt\HortiSem\data")
-inputfile_path = base_path/'dictionary_BOD_csv'
-# get a list of all the files with .dsv
-all_csv_files = inputfile_path.glob('*.csv')
-
-for f in all_csv_files:   
-    reader = csv.DictReader(open(f, 'r'),delimiter=',')   
-    pattern_terms=[row["KODETEXT"] for row in  reader]
-    entity_matcher = EntityMatcher(nlp, pattern_terms, f"{f.stem}")
-    nlp.add_pipe(entity_matcher, name=f"entity_matcher_{f.stem}",after="ner")
-
-def add_regex_entities(doc):
-    label = "Zeit"
-    regex_expression = r"\d{1,2}\.\d{1,2}\.\d{4}"
-    spans = []
-    for match in re.finditer(regex_expression, doc.text):  # find match in text
-        start, end = match.span()  # get the matched token indices
-        span = doc.char_span(start, end, label=label)
-        spans.append(span)
-    doc.ents = list(doc.ents) + spans
-    return doc
-
-nlp.add_pipe(add_regex_entities, name="add_regex_match",before="ner")
-
-# def add_regex_entities(doc):
-#     regex_expression = r"BBCH(\s?\d+)\s?(\/|\-|(bis)?)\s?(\d+)?"
-#     spans = []
-#     for match in re.finditer(regex_expression, doc.text):  # find match in text
-#         start, end = match.span()  # get the matched token indices
-#         span = doc.char_span(start, end, label="BBCH_Stadium")
-#         spans.append(span)
-#     doc.ents = list(doc.ents) + spans
-#     return doc
-
-# add custom components
-# nlp.add_pipe(add_regex_entities, name="add_regex_match",before="ner")
 
 def pdf_converter(pdf_path):
     # Read pdf and convert to plain text
@@ -125,42 +75,6 @@ async def create_upload_file(file: UploadFile = File(...)):
     print(ents)
     return {"filename": file.filename, "text":input_text,"ents":ents}
     
-# @app.post(
-#     "/entities_by_type", response_model=RecordsEntitiesByTypeResponse, tags=["NER"]
-# )
-# async def extract_entities_by_type(body: RecordsRequest):
-#     """Extract Named Entities from a batch of Records separated by entity label.
-#         This route can be used directly as a Cognitive Skill in Azure Search
-#         For Documentation on integration with Azure Search, see here:
-#         https://docs.microsoft.com/en-us/azure/search/cognitive-search-custom-skill-interface"""
-
-#     res = []
-#     documents = []
-
-#     for val in body.values:
-#         documents.append({"id": val.recordId, "text": val.data.text})
-
-#     entities_res = extractor.extract_entities(documents)
-#     res = []
-
-#     for er in entities_res:
-#         groupby = defaultdict(list)
-#         for ent in er["entities"]:
-#             ent_prop = ENT_PROP_MAP[ent["label"]]
-#             groupby[ent_prop].append(ent["name"])
-#         record = {"recordId": er["id"], "data": groupby}
-#         res.append(record)
-
-#     return {"values": res}    
-
-# @app.post("/uploadfile/")
-# async def create_upload_file(file: UploadFile = File(...)):
-#     contents = await file.read()
-#     blob = base64.b64decode(contents)
-#     with open(os.path.join(UPLOAD_FOLDER, file.filename), 'wb') as outfile:
-#         outfile.write(blob)
-#     return {"filename": file.filename}
-
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
--- ml.py
+++ ml.py
@@ -1,3 +1,90 @@
-import spacy
+from spacy.lang.de import German
+from spacy.pipeline import EntityRuler
+from spacy.matcher import PhraseMatcher
+from spacy.matcher import Matcher
+from spacy.tokens import Span
 
-nlp = spacy.load("M:/Projekt/HortiSem/tmp_model")
+from pathlib import Path
+import ujson
+import jsonlines
+
+nlp = German()
+ruler = EntityRuler(nlp)
+# nlp = spacy.load("M:/Projekt/HortiSem/tmp_model_v1")
+
+
+
+# define the file path and output file path
+base_path = Path("M:\Projekt\HortiSem")
+patterns_path = base_path/'data/dictionary_jsonl'
+
+# get a list of all the files with .dsv
+all_pattern_files = patterns_path.glob('*.jsonl')
+
+for f in all_pattern_files:
+    with jsonlines.open(f) as reader:
+        ruler.add_patterns(reader)
+        nlp.add_pipe(ruler,name=f"entity_ruler_{f.stem}")
+
+
+print(nlp.pipeline)
+
+# class EntityMatcher(object):
+#     # name = "entity_matcher"
+
+#     def __init__(self, nlp, terms, label):
+#         patterns = [nlp.make_doc(text) for text in terms]
+#         self.matcher = PhraseMatcher(nlp.vocab)
+#         self.matcher.add(label, None, *patterns)
+
+#     def __call__(self, doc):
+#         matches = self.matcher(doc)
+#         for match_id, start, end in matches:
+#             span = Span(doc, start, end, label=match_id)
+#             doc.ents = list(doc.ents) + [span]
+#         return doc
+
+
+# # for f in all_csv_files:   
+# #     reader = csv.DictReader(open(f, 'r'),delimiter=',')   
+# #     pattern_terms=[row["KODETEXT"] for row in  reader]
+# #     entity_matcher = EntityMatcher(nlp, pattern_terms, f"{f.stem}")
+# #     nlp.add_pipe(entity_matcher, name=f"entity_matcher_{f.stem}",after="ner")
+
+# def add_regex_entities(doc):
+#     label_z = "Zeit"
+#     regex_expression_z = r"\d{1,2}\.\d{1,2}\.\d{2,4}"
+    
+#     spans = []
+#     for match in re.finditer(regex_expression_z, doc.text):  # find match in text
+#         start, end = match.span()  # get the matched token indices
+#         span = doc.char_span(start, end, label=label_z)
+#         spans.append(span)
+        
+#     label_b = "BBCH_Stadium"
+#     regex_expression_b = r"BBCH(\s?\d+)\s?(\/|\-|(bis)?)\s?(\d+)?"
+#     for match in re.finditer(regex_expression_b, doc.text):  # find match in text
+#         start, end = match.span()  # get the matched token indices
+#         span = doc.char_span(start, end, label=label_b)
+#         spans.append(span)
+
+#     doc.ents = list(doc.ents) + spans
+#     return doc
+
+# nlp.add_pipe(add_regex_entities, name="add_regex_match",before="ner")
+
+# # def add_regex_entities(doc):
+#     regex_expression = r"BBCH(\s?\d+)\s?(\/|\-|(bis)?)\s?(\d+)?"
+#     spans = []
+#     for match in re.finditer(regex_expression, doc.text):  # find match in text
+#         start, end = match.span()  # get the matched token indices
+#         span = doc.char_span(start, end, label="BBCH_Stadium")
+#         spans.append(span)
+#     doc.ents = list(doc.ents) + spans
+#     return doc
+
+doc = nlp("BBCH 15, Fliegen, Flugbrand . Brandenburg, in Berlin, Schnecken")
+
+print([(ent.text, ent.label_) for ent in doc.ents])
+
+nlp.to_disk(base_path/"rule_model")
